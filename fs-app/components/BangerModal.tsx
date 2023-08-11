d29cd414121e3fd3e3a1be8ebc4034fdbe22c3ba
realisticattorney <g.axl.aquila@gmail.com>
2023-08-11 13:01:26 -0400

    Add nonstateful history of text-banger tweets

--- fs-app/components/BangerModal.tsx
+++ fs-app/components/BangerModal.tsx
@@ -13,19 +13,53 @@ export default function BangerModal() {
     isLoading,
   } = useChat({
     api: "/api/banger",
-    // initialMessages: [
-    //   {
-    //     content:
-    //       "Sorry babe I can't listening how was your day ever again. I lose my edge context-switching to your toxic work environment",
-    //     role: "user",
-    //     id: "ISgO141",
-    //   },
-    //   {
-    //     id: "XEAnGmj",
-    //     content:
-    //       "\"I just saw someone walking around with a sign that said 'I'm an undefined variable' and I couldn't help but think, same.",
-    //   },
-    // ],
+    initialMessages: [
+      {
+        content:
+          "Sorry babe I can't listening how was your day ever again. I lose my edge context-switching to your toxic work environment",
+        role: "user",
+        id: "ISgO141",
+      },
+      {
+        id: "XEAnGmj",
+        content:
+          "\"I just saw someone walking around with a sign that said 'I'm an undefined variable' and I couldn't help but think, same.",
+        role: "system",
+      },
+      {
+        content: "I dont know mannn\n",
+        role: "user",
+        id: "TftCoI2",
+      },
+      {
+        id: "dnE9T9I",
+        content:
+          '"Just found out my ex was an alien the whole time. Talk about out of this world!"',
+        role: "assistant",
+      },
+      {
+        content: "thanks broter\n",
+        role: "user",
+        id: "JSK2xq1",
+      },
+      {
+        id: "BgeGxWj",
+        content:
+          '"Ducks are so weird. I mean, have you ever seen one wearing pants?',
+        role: "assistant",
+      },
+      {
+        content: "hehehehehehehehehe",
+        role: "user",
+        id: "5KyZadz",
+      },
+      {
+        id: "3WrgVR6",
+        content:
+          '"My therapist just told me I have an unhealthy obsession with the Oxford comma. I think it\'s time to take a break from our sessions."',
+        role: "assistant",
+      },
+    ],
   });
   console.log(messages);
   return (
@@ -71,9 +105,27 @@ export default function BangerModal() {
             </button>
           </form>
           {isLoading && <p>generating a banger...</p>}
-          <div className="w-1/3 m-auto text-center pt-5">
-            {messages.map((m) => (
-              <div key={m.id}>
+          <div
+            className="m-auto pt-5 mt-4 rounded-lg"
+            style={{
+              maxWidth: "500px",
+              height: "400px",
+              overflowY: "auto",
+              overflowX: "hidden",
+            }}
+          >
+            {messages.map((m, index) => (
+              <div
+                key={m.id}
+                className={`p-2 text-left my-2 text-gray-300 ${
+                  m.role === "user" ? "ml-5" : "mr-5"
+                }`}
+                style={{
+                  backgroundColor: index % 2 === 0 ? "#092c4949" : "#094b8149",
+                  borderRadius: "10px",
+                }}
+              >
+                <strong>{m.role === "user" ? "User" : "AI"}:</strong>{" "}
                 {m.content}
               </div>
             ))}
